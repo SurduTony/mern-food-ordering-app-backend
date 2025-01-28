550da2f5f668e031fbc88c16d639bbe162f21a8d
tony <surdutony321@yahoo.com>
2025-01-28 14:23:54 +0200

    adding user profile feature

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/database";
+import { v2 as cloudinary } from "cloudinary";
+
 import myUserRoute from "./routes/MyUser.routes";
+import myRestaurantRoute from "./routes/MyRestaurant.routes";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -14,6 +23,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
